124e4a6cd6ac96069eb089f3ab9ed0ba414a7fe6
Vuyisanani <vmeteni@gmail.com>
2020-04-21 05:30:52 +0200

    Removed unused file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var fs = require("fs");
 var dataJson = fs.readFileSync("data.json", 'UTF-8');
 var geoJson = fs.readFileSync("geo.json", 'UTF-8');
-// const test = JSON.parse(dataJson);
 var isValidJSON = function (data) {
     if ((String(data).length === 0)) {
         return "The length of the Object is " + String(data).length;
@@ -9,7 +8,7 @@ var isValidJSON = function (data) {
     try {
         var _data = JSON.parse(data);
         if (_data && typeof _data === "object" && (_data instanceof Array)) {
-            console.table(_data);
+            // To Do: Call the functions to return data.json
             return "Parsed the JSON file:";
         }
     }
@@ -18,17 +17,3 @@ var isValidJSON = function (data) {
     }
     return "The file you have added is ot a valid JSON file";
 };
-console.log(isValidJSON(geoJson));
-// let distance = (lat1: number, lon1: number, lat2: number, lon2: number) => {
-// 	if ((lat1 == lat2) && (lon1 == lon2)) {
-// 		return 0;
-// 	}
-// 	else {
-// 		let dist = Math.sqrt((lat2 - lat1)**2 + (lon2 - lon1)**2)
-// 		return dist;
-// 	}
-// }
-// console.log('distance: ', distance(1, 4, 3, 4));
-// let res = function(num1: number, num2: number) {
-//     let dist = Math.sqrt((num1)**2 - (num2)**2);
-// }
